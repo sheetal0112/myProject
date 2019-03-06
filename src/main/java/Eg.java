cad16a949baf1108bc458fab560149ceb6d000f7
sheetal0112 <sheetal.agarwal@tothenew.com>
2019-03-06 14:57:42 +0530

    gradle

---
+++ src/main/java/Eg.java
@@ -0,0 +1,5 @@
+public class Eg{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java2/Egg2.java
@@ -0,0 +1,5 @@
+public class Egg2{
+    public static void main(String[] args) {
+        System.out.println("Tu beer hai");
+    }
+}
